a3726e4327f87d8abc12bceefd12964335c16528
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-02-21 23:24:58 -0600

    install clarifai(AI API). Adding face recognition componenet

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
-import Navigation from './components/navigation/Navigation';
-import Logo from './components/logo/Logo';
-import Rank from './components/rank/Rank';
-import './App.css';
-import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
-import Particles from 'react-particles-js';
-
+import React from "react";
+import Clarifai from "clarifai";
+import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import Rank from "./components/rank/Rank";
+import "./App.css";
+import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
+import Particles from "react-particles-js";
+import FaceRecognition from "./components/facerecognition/FaceRecognition";
 
 const particlesOptions = {
   particles: {
-      number:{
-        value:30,
-        density:{
-          enable:true, 
-          value_area:300,
-        }
-    }
-  }
-}
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 300,
+      },
+    },
+  },
+};
+const app = new Clarifai.App({
+  apiKey: "57985cb41cb449de9ed182ecf9672d6a",
+});
 
-function App() {
-  return (
-    <div className="App">
-                  <Particles 
-                className = "particles"
-                params={particlesOptions} />
-      <Navigation/>
-      <Logo/>
-     <Rank/>
-     <ImageLinkForm/>
-       {/*<FaceRecognition/>*/}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        // THE JPG
+        this.state.input
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/facerecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaceRecognition = () => {
+  return (
+    <div >
+    bla
+        <img src= {'https://media.2oceansvibe.com/wp-content/uploads/2020/08/tony-hawk.jpg'} alt = 'face recognition'/>
+    </div>
+  );
+};
+export default FaceRecognition;
